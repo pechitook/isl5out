ad63504ba027dc04140334153fb4f51e8269d3f5
Pablo Chiappetti <pablo@tril.us>
2015-02-02 10:49:47 -0300

    add github ribbon

--- app/views/no.blade.php
+++ app/views/no.blade.php
@@ -20,8 +20,11 @@
         </style>
     </head>
     <body>
+        <a href="https://github.com/p4bloch/isl5out"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
         <h1>Not yet :(</h1>
         <p>This is updated every minute. Keep this tab open :)</p>
-        <p style="margin-top: 40px;">Made with <span style="color:rgb(181, 64, 64)"><3</span> by <a href="//twitter.com/p4bloch" style="color:#aaa;text-decoration: none">@p4bloch</a></p>
+        <p style="margin-top: 40px;">Made with <span style="color:rgb(181, 64, 64)"><3</span> by <a href="//twitter.com/p4bloch" style="color:#aaa;text-decoration: none">@p4bloch</a>
+        </p>
+        <p></p>
     </body>
 </html>
